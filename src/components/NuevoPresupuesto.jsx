bb8043cd0bb8e82486589d9e9aed95976a9c5c55
adrianarmaza <adrianarmaza@gmail.com>
2023-09-29 01:32:02 -0300

    29-09

--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Mensaje from './Mensaje'
 
 const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
+
+  const[mensaje, setMensaje] = useState('')
+
+  const handlePresupuesto = (e) => {
+    e.preventDefault();
+    if(!Number(presupuesto) || Number(presupuesto) <0 ){
+      setMensaje('no es un presupuesto valido')
+    }else{
+      setMensaje('si es un presupuesto valido')
+    }
+
+
+  }
+
   return (
     <div className='contenedor-presupuesto contenedor sombra'>
-        <form className='formulario'>
+        <form onSubmit={handlePresupuesto} className='formulario'>
             <div className='campo'>
                 <label>Definir Presupuesto</label>
 
@@ -18,6 +33,7 @@ const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
 
             <input type='submit' value='Añadir'>
             </input>
+            {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
 
         </form>
     </div>
